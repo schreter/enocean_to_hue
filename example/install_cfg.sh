ab3282da6d84753f646e6cc45363b85fadf8fe1d
Ivan Schreter <schreter@gmx.net>
2020-12-27 19:09:42 +0100

    Update example - new sensor

---
+++ example/install_cfg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Destination machine where to install the executable
+DEST=dm920
+# Configuration to use
+CONFIG=example/example.conf
+
+# we expect the build directory to be at ../build.arm
+ROOT=`dirname $0`/..
+cd ${ROOT}/../build.arm || (echo "Could not find ARM build directory"; exit 1)
+
+SOURCE=`grep enocean_to_hue_SOURCE_DIR CMakeCache.txt | cut -f2 -d=` || (echo "Cannot determine source directory"; exit 1)
+
+scp ${SOURCE}/example/example.conf ${DEST}:~ && ssh ${DEST} ./install_enocean.sh
